82ccefc3fc3efc33489e5f0253327866328e18c3
Pigi <165502530+pierkalac@users.noreply.github.com>
2025-04-24 11:31:52 +0200

    Update and rename sconto.py to promo.py

--- promo.py
+++ promo.py
@@ -1,4 +1,4 @@
-# sconto.py
+# promo.py
 def main():
     prezzi = []
     animale = []
@@ -11,15 +11,15 @@ def main():
     sconto = calcola_sconto(prezzi, animale, len(prezzi))
     print(f"Sconto applicato: € {sconto:.2f}")
 
-def calcola_sconto(prices, isPet, nr_articoli):
-    nr_animali = sum(isPet)
+def calcola_sconto(prezzo, e_animale, nr_articoli):
+    nr_animali = sum(e_animale)
     nr_non_animali = nr_articoli - nr_animali
 
     if nr_animali >= 1 and nr_non_animali >= 5:
         sconto = 0
         for i in range(nr_articoli):
-            if not isPet[i]:
-                sconto += prices[i] * 0.2
+            if not e_animale[i]:
+                sconto += prezzo[i] * 0.2
         return sconto
     else:
         return 0
